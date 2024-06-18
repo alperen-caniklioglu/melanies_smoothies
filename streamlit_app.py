41aad9be14623bb3aa245d88a59abcf22e5cb0e2
alperen <106776036+alperen-caniklioglu@users.noreply.github.com>
2024-06-19 01:02:02 +0200

    Update streamlit_app.py
    Development check: Convert snowpark dataframe to pandas dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 import requests
+import pandas as pd
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -17,9 +18,13 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe (data=my_dataframe, use_container_width=True)
-st.stop()
+#st.dataframe (data=my_dataframe, use_container_width=True)
+#st.stop()
 
+# Convert the Snowpark Dataframe to a Pandas Dataframe to use the LOC function
+pf_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 
 
 name_on_order = st.text_input("Name on Smoothie:")
