4c2ee666ceea220004056aca9eb38448c1dbc970
alperen <106776036+alperen-caniklioglu@users.noreply.github.com>
2024-06-19 01:22:20 +0200

    Update streamlit_app.py
    Test snowflake dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,9 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        
+        #search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        search_on=my_dataframe.filter('FRUIT_NAME'==fruit_chosen).select(col('SEARCH_ON'))
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
         st.subheader(fruit_chosen + ' Nutrition Information')
